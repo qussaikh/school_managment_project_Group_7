27c78b75a9c44b6c89f3310c239b60325e7bff65
xvickyx96 <96viksin@gafe.molndal.se>
2024-03-21 14:34:49 +0100

    ändrat url

--- src/main/java/com/vaaq/school/student/controller/StudentController.java
+++ src/main/java/com/vaaq/school/student/controller/StudentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/student")
+@RequestMapping("/api/v1/student")
 public class StudentController {
 
     @Autowired
